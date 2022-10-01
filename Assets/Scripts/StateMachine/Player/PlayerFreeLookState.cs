8e81fb1c89d23224450a1d4db1c3851e08a13685
Alexander Medved <medved1991alexander@gmail.com>
2022-10-02 00:28:22 +0300

    refactoring and smooth rotation knight

---
+++ Assets/Scripts/StateMachine/Player/PlayerFreeLookState.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerFreeLookState : PlayerBaseState
+{  
+    private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed"); 
+    private const float AnimatorDampTime = 0.1f;
+
+    public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        Vector3 movement = CalculateMovement();
+               
+        if(stateMachine.InputReader.MovementValue != Vector2.zero)
+        {
+            stateMachine.Controller.Move(movement * stateMachine.FreeLookMovementSpeed * deltaTime);
+            FaceMovementDirection(movement, deltaTime);
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 1, AnimatorDampTime, deltaTime);
+        }
+        else
+        {
+            stateMachine.Animator.SetFloat(FreeLookSpeedHash, 0, AnimatorDampTime, deltaTime);
+        }       
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y + right * stateMachine.InputReader.MovementValue.x;
+    }
+
+    private void FaceMovementDirection(Vector3 movement, float deltaTime)
+    {
+        stateMachine.transform.rotation = Quaternion.Lerp(
+            stateMachine.transform.rotation,
+            Quaternion.LookRotation(movement),
+            deltaTime * stateMachine.RotationDamping);
+         
+    }
+
+}
+
